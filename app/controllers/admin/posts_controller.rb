d0896cd2fdcb5c562fa5192218bb8cc30916c07c
Andrey Kulakevich <andrey.kulakevich@gmail.com>
2017-12-10 21:12:42 -0400

    Add some basic structure.

---
+++ app/controllers/admin/posts_controller.rb
@@ -0,0 +1,5 @@
+class Admin::PostsController < ApplicationController
+  def index
+    @posts = Post.all.page(params[:page])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  devise_for :users
+
+  root to: 'posts#index'
+
+  namespace :admin do
+    resources :posts do
+      resources :comments, only: [:index, :destroy], shallow: true
+    end
+  end
+
+  resources :posts, only: [:index, :show] do
+    resources :comments, shallow: true, only: [:index, :create, :update, :destroy]
+  end
 end
